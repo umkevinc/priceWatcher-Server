375f97035d03a090720a3463e7d1e5f16e811c25
btswt22 <sherry415@gmail.com>
2014-12-06 23:12:46 -0600

    updated configs.py

--- crawler/src/pricewatcher/configs.py
+++ crawler/src/pricewatcher/configs.py
@@ -21,3 +21,34 @@ F21_CATEGORY_LIST = [
 	('f21', 'top'), 
 	('f21', 'whatsnew_all') # New Arrival		
 ]
+
+JCREW_BASE_URL = 'http://www.jcrew.com/'
+JCREW_CATEGORY_LIST = [
+	('womens_category', 'sweaters'),
+	('womens_category', 'knitstees'),
+	('womens_category', 'shirtsandtops'),
+	('womens_category', 'denim'),
+	('womens_category', 'pants'),
+	('womens_category', 'outerwear'),
+	('womens_category', 'blazers'),
+	('womens_category', 'dresses'),
+	('womens_category', 'shirts'),
+	('womens_category', 'shorts'),
+	('womens_category', 'suiting'),
+	('womens_category', 'weddingsandparties'),
+	('womens_category', 'sleepwear'),
+	('womens_category', 'swim'),
+	('womens_category', 'beachcoverups'),
+	('womens_category', 'maternity'),
+	('womens_category', 'shoes'),
+	('womens_category', 'jewelry'),
+	('womens_category', 'finejewelry'),
+	('womens_category', 'handbags'),
+	('womens_category', 'accessories'),
+	('womens_category', 'eyewear'),
+	('womens_category', 'homeandgift'),
+	('womens_category', 'beauty'),
+	('womens_category', 'intimates'),
+	('womens_category', 'ingoodcompany'),
+	('womens_category', 'collection')
+]
