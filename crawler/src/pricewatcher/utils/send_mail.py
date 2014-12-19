0af9d4c720affd8fac8faf22d905a1a6bc0374f9
Kevin Cheng <kevin.cheng76@gmail.com>
2014-12-18 21:32:55 -0600

    upate cralwer_report

--- crawler/src/pricewatcher/utils/send_mail.py
+++ crawler/src/pricewatcher/utils/send_mail.py
@@ -25,7 +25,8 @@ class PriceWatcherServerMail(object):
         self._smtpserver.ehlo
         self._smtpserver.login(self._user, self._pass)
 
-    def send(self, subject, msg, to_list=self._to_list):
+    def send(self, subject, msg, to_list=None):
+        to_list = self._to_list if to_list is None else to_list
         subject = 'To:' + ','.join(to_list) + '\n' + 'From: ' + self._user + '\n'
         print subject
         self._smtpserver.sendmail(self._user, to_list[0], msg)
